9fc3f7f9bc6fcf3477f55068320ddbcfb5e7a680
irfanprabaswara <irfanprabaswara@gmail.com>
2021-11-03 01:21:11 +0800

    bug fix register dependencies

--- routes/web.php
+++ routes/web.php
@@ -28,6 +28,10 @@ Route::get('/accept/{id}','UsersController@approve');
 Route::group(['middleware' => ['guest']], function () {
     Route::get('/login', 'Auth\LoginControl@get_login');
     Route::post('/login', 'Auth\LoginControl@post_login');
+
+    //register user baru
+Route::get('/register', 'UserPengguna\UserPenggunaControl@insert_user');
+Route::post('/register/post_user', 'UserPengguna\UserPenggunaControl@post_user');
 });
 
 //Logout
